7f2acfeba89ccfca8f363939dfdc6869a523cd6b
IgorDAM <151687081+IgorDAM@users.noreply.github.com>
2025-04-15 20:53:07 +0200

    Update gulpfile.js

--- gulpfile.js
+++ gulpfile.js
@@ -3,65 +3,51 @@ const cleanCSS = require('gulp-clean-css');
 const uglify = require('gulp-uglify');
 const imagemin = require('gulp-imagemin');
 
-// Tarea para optimizar CSS
-function minifyCss() {
-    return gulp.src('css/*.css')
-        .pipe(cleanCSS({compatibility: 'ie8'}))
-        .pipe(gulp.dest('dist/css'));
+// Tarea para CSS
+function css() {
+  return gulp.src('css/*.css')
+    .pipe(cleanCSS({ compatibility: 'ie8' }))
+    .pipe(gulp.dest('dist/css'));
 }
 
-// Tarea para optimizar JavaScript
-function minifyJs() {
-    return gulp.src('js/*.js')
-        .pipe(uglify())
-        .pipe(gulp.dest('dist/js'));
+// Tarea para JS
+function js() {
+  return gulp.src('js/*.js')
+    .pipe(uglify())
+    .pipe(gulp.dest('dist/js'));
 }
 
-// Tarea para optimizar imágenes
-function optimizeImages() {
-    return gulp.src('img/*')
-        .pipe(imagemin([
-            imagemin.gifsicle({interlaced: true}),
-            imagemin.mozjpeg({quality: 75, progressive: true}),
-            imagemin.optipng({optimizationLevel: 5}),
-            imagemin.svgo({
-                plugins: [
-                    {removeViewBox: true},
-                    {cleanupIDs: false}
-                ]
-            })
-        ]))
-        .pipe(gulp.dest('dist/img'));
+// Tarea para imágenes
+function images() {
+  return gulp.src('img/*')
+    .pipe(imagemin([
+      imagemin.gifsicle({ interlaced: true }),
+      imagemin.mozjpeg({ quality: 75, progressive: true }),
+      imagemin.optipng({ optimizationLevel: 5 }),
+      imagemin.svgo({
+        plugins: [
+          { removeViewBox: true },
+          { cleanupIDs: false }
+        ]
+      })
+    ]))
+    .pipe(gulp.dest('dist/img'));
 }
 
-// Tarea para copiar archivos HTML
-function copyHtml() {
-    return gulp.src('*.html')
-        .pipe(gulp.dest('dist'));
+// Tarea para HTML
+function html() {
+  return gulp.src('*.html')
+    .pipe(gulp.dest('dist'));
 }
 
-// Tarea para copiar fuentes
-function copyFonts() {
-    return gulp.src('fonts/*')
-        .pipe(gulp.dest('dist/fonts'));
+// Tarea para fuentes
+function fonts() {
+  return gulp.src('fonts/*')
+    .pipe(gulp.dest('dist/fonts'));
 }
 
-// Tarea para desarrollo que observa cambios
-function watchFiles() {
-    gulp.watch('css/*.css', minifyCss);
-    gulp.watch('js/*.js', minifyJs);
-    gulp.watch('img/*', optimizeImages);
-    gulp.watch('*.html', copyHtml);
-}
-
-const build = gulp.parallel(
-    minifyCss,
-    minifyJs,
-    optimizeImages,
-    copyHtml,
-    copyFonts
-);
+// Tarea principal
+const build = gulp.parallel(css, js, images, html, fonts);
 
 exports.default = build;
 exports.build = build;
-exports.watch = watchFiles;
